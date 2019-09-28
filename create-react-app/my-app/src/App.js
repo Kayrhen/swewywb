088c31d1cd58b720dd6f84856ded0b688b85f3db
Bryce Dai <32209819+brycedai@users.noreply.github.com>
2019-09-27 22:19:49 -0500

    Added Bootstrap

--- create-react-app/my-app/src/App.js
+++ create-react-app/my-app/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Navbar, Jumbotron, Button, NavDropdown } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
